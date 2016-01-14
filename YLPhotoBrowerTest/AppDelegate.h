836f4d5cba2f5f7c353244ceeeed5917d780500f
test <test@example.com>
2016-01-14 10:23:37 -0800

    Initial Commit

---
+++ YLPhotoBrowerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLPhotoBrowerTest
+//
+//  Created by 有 on 16/1/14.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLPhotoBrowerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLPhotoBrowerTest
+//
+//  Created by 有 on 16/1/14.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
